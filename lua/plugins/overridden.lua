c110b93f76b44d2a0fcf94f3c2f75cbf1a46dd95
Hayden Phothong <hphothong@gmail.com>
2025-04-27 09:19:31 -0700

    feat(plugins): add ts lang server, eslint, and prettier to mason
    This change adds the common language servers for Mason package
    manager. These defaults will be loaded when neovim first launches.

--- lua/plugins/overridden.lua
+++ lua/plugins/overridden.lua
@@ -4,9 +4,7 @@ return {
     opts = function(_, opts)
       -- list like portions of a table cannot be merged naturally and require the user to merge it manually
       -- check to make sure the key exists
-      if not opts.ensure_installed then
-        opts.ensure_installed = {}
-      end
+      if not opts.ensure_installed then opts.ensure_installed = {} end
       vim.list_extend(opts.ensure_installed, {
         "lua",
         "vim",
@@ -21,5 +19,29 @@ return {
         "yaml",
       })
     end,
-  }
+  },
+  -- use mason-tool-installer for automatically installing Mason packages
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    -- overrides `require("mason-tool-installer").setup(...)`
+    opts = {
+      -- Make sure to use the names found in `:Mason`
+      ensure_installed = {
+        -- install language servers
+        "lua-language-server",
+        "typescript-language-server",
+
+        -- install formatters
+        "stylua",
+        "eslint_d",
+        "prettierd",
+
+        -- install debuggers
+        -- "debugpy",
+
+        -- install any other package
+        "tree-sitter-cli",
+      },
+    },
+  },
 }
