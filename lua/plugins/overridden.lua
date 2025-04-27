fd6af627beae130b5fbe548263e60e80bffd5404
Hayden Phothong <hphothong@gmail.com>
2025-04-27 09:12:28 -0700

    feat(plugins): add tree-sitter default overrides
    This change adds some default languages to install
    for the tree-sitter plugin. Most of these are
    web languages that are not installed by default.

---
+++ lua/plugins/overridden.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      -- list like portions of a table cannot be merged naturally and require the user to merge it manually
+      -- check to make sure the key exists
+      if not opts.ensure_installed then
+        opts.ensure_installed = {}
+      end
+      vim.list_extend(opts.ensure_installed, {
+        "lua",
+        "vim",
+        -- add more arguments for adding more treesitter parsers
+        "javascript",
+        "typescript",
+        "html",
+        "css",
+        "json",
+        "markdown",
+        "toml",
+        "yaml",
+      })
+    end,
+  }
+}
